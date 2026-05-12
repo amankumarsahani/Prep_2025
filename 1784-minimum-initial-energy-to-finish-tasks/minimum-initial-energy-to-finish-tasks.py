e5518ae29cbbfdf70eb2949a505c9ed03134a51b
Aman Kumar Sahani <71867895+amankumarsahani@users.noreply.github.com>
2026-05-12 21:44:47 +0530

    Time: 75 ms (100.00%) | Memory: 47 MB (100.00%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minimumEffort(self, tasks):
+        """
+        :type tasks: List[List[int]]
+        :rtype: int
+        """
+
+        # Tasks with larger buffer first
+        tasks.sort(key=lambda x: (x[1] - x[0]), reverse=True)
+
+        ans = 0
+        energy = 0
+
+        for actual, minimum in tasks:
+
+            # If current energy is insufficient,
+            # add more initial energy
+            if energy < minimum:
+                ans += (minimum - energy)
+                energy = minimum
+
+            # Finish the task
+            energy -= actual
+
+        return ans
